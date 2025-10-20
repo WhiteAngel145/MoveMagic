44f944f040f640e20ab140e11c7b7b2efaa7459f
Georgi <ridriruners62@gmail.com>
2025-10-20 13:01:22 +0100

    Add Handlebars as view engine and update routes
    - Added express-handlebars as a dependency in package.json.
    - Configured Handlebars as the view engine in index.js.
    - Updated the home route to render the home.hbs view instead of sending a plain text response.
    - Removed old HTML views (404.html, about.html, create.html, details.html, home.html, search.html) and replaced them with Handlebars templates.
    - Created new Handlebars templates for home, about, create, details, and search pages.
    - Updated static file serving to ensure proper loading of assets.

--- README.md
+++ README.md
@@ -3,11 +3,12 @@ Soft uni exercise js back-end.
 ## Development Steps
 
  - [x] Initialize Project
- - [x] Add Express Server
- - [] Add Workshop Resources
- - [] Setup Handlebars
- - [] Render Home Page
- - [] 
+ - [x] Add Express Server `npm i express`
+ - [x] Add Workshop Resources
+ - [x] Set up Handlebars `npm i express-handlebars`
+ - [x] Set up static files
+ - [x] Render Home Page
+ - [] Config debugging
  - [] 
  - [] 
  - [] 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
 const PORT = 5000;
 
+// Set up Handlebars as the view engine
+app.engine('hbs', handlebars.engine({
+	extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs');
+
+app.set('views', 'src/views');
+
+// Set up middleware to serve static files
+
+app.use(express.static('src/static'));
+
+// Routes
 app.get('/', (req, res) => {
-	res.send('Welcome to MoveMagic!');
+	res.render('home', { layout: false});
 });
 
+// Start the Server
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
